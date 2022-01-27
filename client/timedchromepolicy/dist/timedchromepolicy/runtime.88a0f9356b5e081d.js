2059e191f4b251d523a8aa993a7cf0c7033cc881
Clay Smith <claycodes@google.com>
2022-01-27 08:34:48 -0500

    cross updates

--- client/timedchromepolicy/dist/timedchromepolicy/runtime.88a0f9356b5e081d.js
+++ client/timedchromepolicy/dist/timedchromepolicy/runtime.88a0f9356b5e081d.js
@@ -1,1 +1,14 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 (()=>{"use strict";var e,i={},_={};function n(e){var a=_[e];if(void 0!==a)return a.exports;var r=_[e]={exports:{}};return i[e](r,r.exports,n),r.exports}n.m=i,e=[],n.O=(a,r,c,l)=>{if(!r){var o=1/0;for(f=0;f<e.length;f++){for(var[r,c,l]=e[f],s=!0,t=0;t<r.length;t++)(!1&l||o>=l)&&Object.keys(n.O).every(p=>n.O[p](r[t]))?r.splice(t--,1):(s=!1,l<o&&(o=l));if(s){e.splice(f--,1);var u=c();void 0!==u&&(a=u)}}return a}l=l||0;for(var f=e.length;f>0&&e[f-1][2]>l;f--)e[f]=e[f-1];e[f]=[r,c,l]},n.n=e=>{var a=e&&e.__esModule?()=>e.default:()=>e;return n.d(a,{a}),a},n.d=(e,a)=>{for(var r in a)n.o(a,r)&&!n.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:a[r]})},n.o=(e,a)=>Object.prototype.hasOwnProperty.call(e,a),(()=>{var e={666:0};n.O.j=c=>0===e[c];var a=(c,l)=>{var t,u,[f,o,s]=l,v=0;if(f.some(h=>0!==e[h])){for(t in o)n.o(o,t)&&(n.m[t]=o[t]);if(s)var d=s(n)}for(c&&c(l);v<f.length;v++)n.o(e,u=f[v])&&e[u]&&e[u][0](),e[f[v]]=0;return n.O(d)},r=self.webpackChunktimedchromepolicy=self.webpackChunktimedchromepolicy||[];r.forEach(a.bind(null,0)),r.push=a.bind(null,r.push.bind(r))})()})();
